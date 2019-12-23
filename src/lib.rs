e27d1cb2cd6f863cb4158617b1e7ec8309f7224e
Alex Norman <alex@x37v.info>
2019-12-23 15:03:27 -0800

    moving read into struct

--- src/lib.rs
+++ src/lib.rs
@@ -22,9 +22,87 @@ enum AtsFileType {
 }
 
 struct AtsFile {
-    header: ATS_HEADER,
-    peaks: Vec<Vec<Peak>>,
-    noise: Option<Vec<[f64; NOISE_BANDS]>>,
+    pub header: ATS_HEADER,
+    pub peaks: Vec<Vec<Peak>>,
+    pub noise: Option<Vec<[f64; NOISE_BANDS]>>,
+    pub file_type: AtsFileType,
+}
+
+impl AtsFile {
+    pub fn try_read<P: AsRef<std::path::Path>>(path: P) -> std::io::Result<Self> {
+        let mut header: std::mem::MaybeUninit<ATS_HEADER> = std::mem::MaybeUninit::uninit();
+        let mut file = File::open(path)?;
+        unsafe {
+            let s = slice::from_raw_parts_mut(
+                &mut header as *mut _ as *mut u8,
+                std::mem::size_of::<ATS_HEADER>(),
+            );
+            file.read_exact(s)?;
+            let header = header.assume_init();
+
+            if header.mag != 123f64 {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidData,
+                    "magic number does not match",
+                ));
+            }
+            let file_type = match header.typ as usize {
+                1 => AtsFileType::AmpFreq,
+                2 => AtsFileType::AmpFreqPhase,
+                3 => AtsFileType::AmpFreqNoise,
+                4 => AtsFileType::AmpFreqPhaseNoise,
+                _ => {
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::InvalidData,
+                        format!("{} type ATS files not supported yet", header.typ),
+                    ))
+                }
+            };
+
+            let mut peaks = Vec::new();
+            let mut noise = Vec::new();
+            for _f in 0..header.fra as usize {
+                //skip frame time
+                file.seek(SeekFrom::Current(std::mem::size_of::<f64>() as i64))?;
+
+                let mut frame_peaks = Vec::new();
+
+                for _p in 0..header.par as usize {
+                    let mut amp_freq = [0f64; 2];
+                    file.read_f64_into::<LittleEndian>(&mut amp_freq)?;
+                    let mut peak = Peak {
+                        amp: amp_freq[0],
+                        freq: amp_freq[1],
+                        phase: None,
+                    };
+                    match file_type {
+                        AtsFileType::AmpFreqPhase | AtsFileType::AmpFreqPhaseNoise => {
+                            peak.phase = Some(file.read_f64::<LittleEndian>()?)
+                        }
+                        _ => (),
+                    }
+                    frame_peaks.push(peak);
+                }
+                match file_type {
+                    AtsFileType::AmpFreqNoise | AtsFileType::AmpFreqPhaseNoise => {
+                        let mut nframe = [0f64; 25];
+                        file.read_f64_into::<LittleEndian>(&mut nframe)?;
+                        noise.push(nframe);
+                    }
+                    _ => (),
+                }
+                peaks.push(frame_peaks);
+            }
+
+            let noise = if noise.len() != 0 { Some(noise) } else { None };
+            Ok(Self {
+                header,
+                peaks,
+                noise,
+                file_type,
+            })
+        }
+    }
 }
 
 struct Peak {
@@ -34,7 +112,6 @@ struct Peak {
 }
 
 external! {
-
     pub struct AtsDump {
         current: Option<AtsFile>,
         outlet: Box<dyn OutletSend>,
@@ -59,76 +136,16 @@ external! {
 
         #[sel]
         pub fn open(&mut self, filename: Symbol) {
-            match self.read_file(&filename) {
-                Ok(_) => self.post(format!("read {}", filename)),
-                Err(err) => self.post(format!("error {}", err))
-            }
-        }
-
-        fn read_file(&mut self, filename: &Symbol) -> std::io::Result<AtsFile> {
-            let mut file = File::open(filename)?;
-            let mut header: std::mem::MaybeUninit<ATS_HEADER> = std::mem::MaybeUninit::uninit();
-            unsafe {
-                let s = slice::from_raw_parts_mut(&mut header as *mut _ as *mut u8, std::mem::size_of::<ATS_HEADER>());
-                file.read_exact(s)?;
-                let header = header.assume_init();
-
-                if header.mag != 123f64 {
-                    return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "magic number does not match"));
-                }
-                let typ = match header.typ as usize {
-                    1 => AtsFileType::AmpFreq,
-                    2 => AtsFileType::AmpFreqPhase,
-                    3 => AtsFileType::AmpFreqNoise,
-                    4 => AtsFileType::AmpFreqPhaseNoise,
-                    _ => return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, format!("{} type ATS files not supported yet", header.typ)))
-                };
-
-                let mut peaks = Vec::new();
-                let mut noise = Vec::new();
-                for f in 0..header.fra as usize {
-                    //skip frame time
-                    file.seek(SeekFrom::Current(std::mem::size_of::<f64>() as i64))?;
-
-                    let mut frame_peaks = Vec::new();
-
-                    for p in 0..header.par as usize {
-                        let mut amp_freq = [0f64; 2];
-                        file.read_f64_into::<LittleEndian>(&mut amp_freq)?;
-                        let mut peak = Peak {
-                            amp: amp_freq[0],
-                            freq: amp_freq[1],
-                            phase: None
-                        };
-                        match typ {
-                            AtsFileType::AmpFreqPhase | AtsFileType::AmpFreqPhaseNoise => peak.phase = Some(file.read_f64::<LittleEndian>()?),
-                            _ => ()
-                        }
-                        frame_peaks.push(peak);
-                    }
-                    match typ {
-                        AtsFileType::AmpFreqNoise | AtsFileType::AmpFreqPhaseNoise => {
-                            let mut nframe = [0f64; 25];
-                            file.read_f64_into::<LittleEndian>(&mut nframe)?;
-                            noise.push(nframe);
-                        }
-                        _ => ()
-                    }
-                    peaks.push(frame_peaks);
-                }
-
-                let noise = if noise.len() != 0 {
-                    Some(noise)
-                } else {
+            self.current = match AtsFile::try_read(filename) {
+                Ok(f) => {
+                    self.post(format!("read {}", filename));
+                    Some(f)
+                },
+                Err(err) => {
+                    self.post(format!("error {}", err));
                     None
-                };
-                Ok(AtsFile{
-                    header,
-                    peaks,
-                    noise
-                })
-            }
+                }
+            };
         }
-
     }
 }
