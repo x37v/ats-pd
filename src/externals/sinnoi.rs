555a42eb6ffb0f046cca3e5112ba1fe406d19733
Alex Norman <alex@x37v.info>
2020-03-08 17:00:11 -0700

    have increment and offset

--- src/externals/sinnoi.rs
+++ src/externals/sinnoi.rs
@@ -157,6 +157,8 @@ impl ParitalSynth {
 pub struct AtsSinNoiProcessor {
     current: Option<Arc<AtsData>>,
     data_recv: Receiver<Command>,
+    incr: ArcAtomic<usize>,
+    offset: ArcAtomic<usize>,
     synths: Box<[ParitalSynth]>,
 }
 
@@ -178,53 +180,82 @@ impl SignalProcessor for AtsSinNoiProcessor {
             }
         }
 
+        let mut clear = || {
+            for out in outputs[0].iter_mut() {
+                *out = 0f32.into();
+            }
+        };
+
         if let Some(c) = &self.current {
             let with_noise = c.has_noise();
             let pmul = c.header.fra / c.header.dur;
-            //TODO handle offset
-            let range = 0..std::cmp::min(c.partials(), self.synths.len());
-            let synths = &mut self.synths[range.clone()];
-            let frames = c.frames.len() as isize;
-            for (out, pos) in outputs[0].iter_mut().zip(inputs[0].iter()) {
-                let pos = (*pos as f64) * pmul;
-                let mut p0 = pos.floor() as isize;
-                let mut fract = 0f64;
-                let mut in_range = false;
-                if p0 < 0 {
-                    p0 = 0;
-                } else if p0 + 1 >= frames {
-                    p0 = frames - 2;
-                    fract = 1f64;
-                } else {
-                    fract = pos.fract();
-                    in_range = true;
-                }
-                let p0 = p0 as usize;
 
-                let f0 = &c.frames[p0];
-                let f1 = &c.frames[p0 + 1];
-                *out = 0 as pd_sys::t_float;
-                for (s, p0, p1) in izip!(
-                    synths.iter_mut(),
-                    f0[range.clone()].iter(),
-                    f1[range.clone()].iter()
-                ) {
-                    let f = lerp(p0.freq, p1.freq, fract);
-                    let (a, n) = if in_range {
-                        (
-                            lerp(p0.amp, p1.amp, fract),
-                            if with_noise {
-                                lerp(p0.noise_energy.unwrap(), p1.noise_energy.unwrap(), fract)
-                            } else {
-                                0f64
-                            },
-                        )
+            let start = self.offset.load(LOAD_ORDERING);
+            let incr = self.incr.load(LOAD_ORDERING);
+            let count = c.partials();
+            if start >= count {
+                clear();
+                return;
+            };
+            let count = count - start;
+            let count = count / incr + if (count % incr) > 0 { 1 } else { 0 };
+
+            //total partials to synthesize
+            let count = std::cmp::min(count, self.synths.len());
+
+            if count == 0 {
+                clear();
+            } else {
+                //end (exclusive) of partial data to synth
+                let end = std::cmp::min(count * incr + start, c.partials());
+                //indexes of partials (step_by later)
+                let range = start..end;
+
+                let synths = &mut self.synths[0..count];
+                let frames = c.frames.len() as isize;
+                for (out, pos) in outputs[0].iter_mut().zip(inputs[0].iter()) {
+                    let pos = (*pos as f64) * pmul;
+                    let mut p0 = pos.floor() as isize;
+                    let mut fract = 0f64;
+                    let mut in_range = false;
+                    if p0 < 0 {
+                        p0 = 0;
+                    } else if p0 + 1 >= frames {
+                        p0 = frames - 2;
+                        fract = 1f64;
                     } else {
-                        (0f64, 0f64)
-                    };
-                    *out = *out + s.synth(f, a, n);
+                        fract = pos.fract();
+                        in_range = true;
+                    }
+                    let p0 = p0 as usize;
+
+                    let f0 = &c.frames[p0];
+                    let f1 = &c.frames[p0 + 1];
+                    *out = 0 as pd_sys::t_float;
+                    for (s, p0, p1) in izip!(
+                        synths.iter_mut(),
+                        f0[range.clone()].iter().step_by(incr),
+                        f1[range.clone()].iter().step_by(incr)
+                    ) {
+                        let f = lerp(p0.freq, p1.freq, fract);
+                        let (a, n) = if in_range {
+                            (
+                                lerp(p0.amp, p1.amp, fract),
+                                if with_noise {
+                                    lerp(p0.noise_energy.unwrap(), p1.noise_energy.unwrap(), fract)
+                                } else {
+                                    0f64
+                                },
+                            )
+                        } else {
+                            (0f64, 0f64)
+                        };
+                        *out = *out + s.synth(f, a, n);
+                    }
                 }
             }
+        } else {
+            clear();
         }
     }
 }
@@ -233,6 +264,8 @@ pd_ext_macros::external! {
     #[name = "ats/sinnoi~"]
     pub struct AtsSinNoiExternal {
         data_send: SyncSender<Command>,
+        incr: ArcAtomic<usize>,
+        offset: ArcAtomic<usize>,
         handles: Box<[ParitalSynthHandle]>,
         post: Box<dyn PdPost>,
     }
@@ -248,6 +281,18 @@ pd_ext_macros::external! {
         #[sel]
         pub fn clear(&mut self) {
             let _ = self.data_send.send(Command::Data(None));
+        }
+
+        #[sel]
+        pub fn offset(&mut self, v: pd_sys::t_float) {
+            let v = std::cmp::max(0, v.floor() as isize) as usize;
+            self.offset.store(v, STORE_ORDERING);
+        }
+
+        #[sel]
+        pub fn incr(&mut self, v: pd_sys::t_float) {
+            let v = std::cmp::max(1, v.floor() as isize) as usize;
+            self.incr.store(v, STORE_ORDERING);
         }
 
         #[sel]
@@ -322,26 +367,68 @@ pd_ext_macros::external! {
         fn new(builder: &mut dyn SignalProcessorExternalBuilder<Self>) -> Result<(Self, Box<dyn SignalProcessor>), String> {
             builder.new_signal_outlet();
             let (data_send, data_recv) = sync_channel(32);
+            let args = builder.creation_args();
 
-            let mut synths = Vec::new();
-            let mut handles = Vec::new();
-            for _ in 0..50 { //TODO get count from args
-                let (h, s) = ParitalSynthHandle::new();
-                handles.push(h);
-                synths.push(s);
+            let mut partials = None;
+            let mut offset = 0;
+            let mut incr = 1;
+
+            //get partial count
+            if args.len() > 0 {
+                if let Some(v) = args[0].get_int() {
+                    partials = if v > 0 { Some(v) } else { None };
+                }
+                if args.len() >= 2 {
+                    if let Some(v) = args[1].get_int() {
+                        if v < 0 {
+                            return Err("offset must be a positive integer".into());
+                        }
+                        offset = v;
+                    }
+                    if args.len() >= 3 {
+                        if let Some(v) = args[2].get_int() {
+                            if v < 1 {
+                                return Err("increment must be an integer greater than 0".into());
+                            }
+                            incr = v;
+                        }
+                    }
+                }
             }
 
-            Ok((
-            Self {
-                data_send,
-                handles: handles.into(),
-                post: builder.poster()
-            },
-            Box::new(AtsSinNoiProcessor {
-                current: None,
-                data_recv,
-                synths: synths.into(),
-            })))
+            let offset = Arc::new(Atomic::new(offset as usize));
+            let incr = Arc::new(Atomic::new(incr as usize));
+
+            if let Some(partials) = partials {
+                let mut synths = Vec::new();
+                let mut handles = Vec::new();
+                for _ in 0..partials {
+                    let (h, s) = ParitalSynthHandle::new();
+                    handles.push(h);
+                    synths.push(s);
+                }
+
+                Ok(
+                    (
+                        Self {
+                            data_send,
+                            handles: handles.into(),
+                            offset: offset.clone(),
+                            incr: incr.clone(),
+                            post: builder.poster()
+                        },
+                        Box::new(AtsSinNoiProcessor {
+                            current: None,
+                            data_recv,
+                            offset,
+                            incr,
+                            synths: synths.into(),
+                        })
+                    )
+                )
+            } else {
+                Err("first argument must be a non zero partial count".into())
+            }
         }
     }
 }
