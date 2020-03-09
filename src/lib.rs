918dea1d63d1bc33d474d6ab722ab9e964d1e1c1
Alex Norman <alex@x37v.info>
2020-03-08 18:35:57 -0700

    use hammer to get help data (though it doesn't work yet)

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ mod cache;
 mod data;
 mod externals;
 
+use std::convert::TryFrom;
+
 extern "C" {
     fn atsdataexternal_setup();
     fn atssinnoiexternal_tilde_setup();
@@ -11,4 +13,11 @@ extern "C" {
 pub unsafe extern "C" fn ats_setup() {
     atsdataexternal_setup();
     atssinnoiexternal_tilde_setup();
+
+    let help =
+        pd_ext::symbol::Symbol::try_from("ats-data-help").expect("failed to create help sym");
+    pd_sys::class_sethelpsymbol(
+        crate::externals::data::ATSDATAEXTERNAL_CLASS.unwrap(),
+        help.inner(),
+    );
 }
