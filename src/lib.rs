a13e15de82161732664d83377be86614a3abce9e
Alex Norman <xnor@perfectco.com>
2020-03-09 13:31:27 -0700

    added sinnoi~ help link

--- src/lib.rs
+++ src/lib.rs
@@ -19,4 +19,9 @@ pub unsafe extern "C" fn ats_setup() {
         crate::externals::data::ATSDATAEXTERNAL_CLASS.unwrap(),
         help.inner(),
     );
+    let help = pd_ext::symbol::Symbol::try_from("ats-sinnoi~").expect("failed to create help sym");
+    pd_sys::class_sethelpsymbol(
+        crate::externals::sinnoi::ATSSINNOIEXTERNAL_CLASS.unwrap(),
+        help.inner(),
+    );
 }
